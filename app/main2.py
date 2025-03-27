2447e0d13299e4f1d46d129b11bb568a9bfdd93c
Ayush Nayal <aynayal@gmail.com>
2025-03-27 14:37:22 +0530

    log dated 27.03.2025

--- app/main2.py
+++ app/main2.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI, HTTPException, Response, status
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # use them without of library module otherwise import will fail
 from .database import SessionDep, create_db_and_tables, select
 from .routers import post, user, auth, vote
 
 app = FastAPI()
+
+origins = [
+    "https://www.google.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # model.SQLModel.metadata.create_all(bind=engine)
 create_db_and_tables()
